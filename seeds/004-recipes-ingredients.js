47d89df348549bf3bbfbd931d036a8a79419a37c
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-09-12 16:02:03 -0700

    recipes-ingredients seed

---
+++ seeds/004-recipes-ingredients.js
@@ -0,0 +1,59 @@
+
+exports.seed = function(knex) {
+  return knex('table_name').insert([
+    {
+      recipe: 1,
+      ingredient: 1,
+      quantity: 1,
+      unit: 'handful'
+    },
+    {
+      recipe: 1,
+      ingredient: 2,
+      quantity: 1,
+      unit: 'whatever'
+    },
+    {
+      recipe: 2,
+      ingredient: 3,
+      quantity: 1,
+      unit: 'whatever'
+    },
+    {
+      recipe: 2,
+      ingredient: 4,
+      quantity: 1,
+      unit: 'whatever'
+    },
+    {
+      recipe: 2,
+      ingredient: 5,
+      quantity: 2,
+      unit: 'slices'
+    },
+    {
+      recipe: 3,
+      ingredient: 6,
+      quantity: 1,
+      unit: 'bottle'
+    },
+    {
+      recipe: 4,
+      ingredient: 7,
+      quantity: 2,
+      unit: 'items'
+    },
+    {
+      recipe: 4,
+      ingredient: 3,
+      quantity: 1,
+      unit: 'whatever'
+    },
+    {
+      recipe: 4,
+      ingredient: 8,
+      quantity: 1,
+      unit: 'whatever'
+    },
+  ]);
+};
